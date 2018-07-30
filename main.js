d40732501cf734fbd79998fc44379bb4e863d7d4
Brian Mathews <brian.mathews@formidable.com>
2018-07-30 08:53:54 -0700

    Avoid using remote in renderer process

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
+const path = require('path');
+const fs = require('fs');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -45,6 +47,31 @@ app.on('activate', function () {
     createWindow()
   }
 })
+
+ipcMain.on("print", (evt, payload) => {
+  const bw = new BrowserWindow();
+
+  bw.webContents.on("did-finish-load", () => {
+    if (payload.type === "pdf") {
+      // .printToPDF(), excessive memory usage
+      bw.webContents.printToPDF({}, (err, data) => {
+        bw.close();
+        const outPath = path.join(__dirname, "out.pdf");
+        fs.writeFile(outPath, data, err => {
+          evt.sender.send("printed", payload);
+        });
+      });
+    } else if (payload.type === "printer") {
+      // .print(), works fine
+      bw.webContents.print({}, () => {
+        bw.close();
+        evt.sender.send("printed", payload);
+      });
+    }
+  });
+
+  bw.loadFile(payload.file);
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,42 +1,32 @@
-const path = require("path");
-const { BrowserWindow } = require("electron").remote;
-const fs = require("fs");
-
-// create bw and load markup.html
-function createAndLoad() {
-  return new Promise(resolve => {
-    const bw = new BrowserWindow();
-    bw.webContents.loadFile("./markup.html");
-    bw.webContents.on("did-finish-load", () => resolve(bw));
-  });
-}
+const { ipcRenderer } = require("electron");
 
 // Uses 4gb of memory
 function printToPDF() {
-  createAndLoad().then(bw => {
-    bw.webContents.printToPDF({}, (err, data) => {
-      bw.close();
-      const outPath = path.join(__dirname, "out.pdf");
-      fs.writeFile(outPath, data);
-      window.alert(`Generated pdf at ${outPath}`);
-    });
+  const jobID = Math.random();
+  ipcRenderer.send("print", {
+    type: "pdf",
+    file: "./markup.html"
   });
 }
 
-// No excess memory when manually saving as pdf
 function print() {
-  createAndLoad().then(bw => {
-    bw.webContents.print({}, () => {
-      bw.close();
-      window.alert("Printed");
-    });
+  const jobID = Math.random();
+  ipcRenderer.send("print", {
+    type: "printer",
+    file: "./markup.html"
   });
 }
+
+ipcRenderer.on("printed", (evt, payload) => {
+  window.alert(`Printed ${JSON.stringify(payload)}`);
+});
 
 document.querySelector("#printToPDF").addEventListener("click", () => {
   printToPDF();
 });
 
+
 document.querySelector("#print").addEventListener("click", () => {
   print();
 });
+
